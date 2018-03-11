e76ad616f805d5bea702391044cb27cc024504f6
Jay Kid <cyph3rfox@gmail.com>
2018-03-11 10:45:50 +0100

    feat: display aria attributes in the inspector too
    + Added some random aria-attrs in the test markup just for the sake of verifying functionality
    + There's too much UI creation code around... will need to extract or try to minimize :(

--- main.js
+++ main.js
@@ -4,10 +4,11 @@
     var STYLE_TAG_ID = 'bfh';
     var INSPECTOR_ELEMENT_ID = 'bfh-inspector';
     var INSPECTOR_TAGNAME_FIELD_ID = 'bfh-inspector-tag';
+    var INSPECTOR_TAG_ATTRIBUTE_LIST_ID = 'bfh-inspector-tag-attr-list';
     var COLOR_TOKEN = 'COLOR';
-    var STYLE_TAG_CONTENTS_TEMPLATE = "*:focus {border: 5px solid " + COLOR_TOKEN + " !important;}";
+    var STYLE_TAG_CONTENTS_TEMPLATE = "*:focus {border: 5px solid " + COLOR_TOKEN + " !important;} input[type=\"radio\"]:focus, input[type=\"checkbox\"]:focus { outline: 5px solid " + COLOR_TOKEN + " !important; }";
 
-    var INSPECTOR_STYLES = "width: 100%;height: 10rem;position: fixed;bottom: 0;background: black;color: white; padding: 1rem;";
+    var INSPECTOR_STYLES = "width: 100%;height: 10rem;position: fixed;bottom: 0;background: rgba(0, 0, 0, 0.5);color: white; padding: 1rem; font-size: 1.4rem";
 
     // Init
     this.currentColorIndex = 0;
@@ -19,7 +20,7 @@
     };
 
     this.generateStyleTagContents = function (color) {
-        var contents = STYLE_TAG_CONTENTS_TEMPLATE.replace(COLOR_TOKEN, color);
+        var contents = STYLE_TAG_CONTENTS_TEMPLATE.replace(COLOR_TOKEN, color).replace(COLOR_TOKEN, color);
         return decodeURIComponent(contents);
     };
 
@@ -47,9 +48,31 @@
         inspectorElementTagNameLabel.innerHTML = "Focused element type: ";
         var inspectorElementTagName = document.createElement('span');
         inspectorElementTagName.setAttribute('id', INSPECTOR_TAGNAME_FIELD_ID);
+        var listElement = document.createElement('ul');
+        listElement.setAttribute('id', INSPECTOR_TAG_ATTRIBUTE_LIST_ID);
         inspectorElement.appendChild(inspectorElementTagNameLabel);
         inspectorElement.appendChild(inspectorElementTagName);
+        inspectorElement.appendChild(listElement);
         document.querySelector('body').appendChild(inspectorElement);
+    };
+
+    this.getAttributesListMarkup = function (DOMNode) {
+        var listElement = document.createElement('ul');
+        var attributesToDisplay = DOMNode.getAttributeNames().filter(function(attributeName) {
+            return attributeName.indexOf('aria') >= 0;
+        });
+        for (var index in attributesToDisplay) {
+            var attributeName = attributesToDisplay[index];
+            var listItemElement = document.createElement('li');
+            var attributeNameElement = document.createElement('span');
+            attributeNameElement.innerText = attributeName + ": ";
+            var attributeValueElement = document.createElement('span');
+            attributeValueElement.innerText = DOMNode.getAttribute(attributeName);
+            listItemElement.appendChild(attributeNameElement);
+            listItemElement.appendChild(attributeValueElement);
+            listElement.appendChild(listItemElement);
+        }
+        return listElement;
     };
 
     // Bindings
@@ -64,9 +87,12 @@
                 var tagNameField = document.querySelector('#'+INSPECTOR_TAGNAME_FIELD_ID);
                 if (tagNameField) {
                     tagNameField.innerHTML = element.tagName;
+                    var attributeListElement = this.getAttributesListMarkup(element);
+                    var tagAttributeListElement = document.querySelector('#'+INSPECTOR_TAG_ATTRIBUTE_LIST_ID);
+                    tagAttributeListElement.innerHTML = attributeListElement.innerHTML;
                 }
             }
-        });
+        }.bind(this));
     };
 
     this.createAndAppendStyleTag();
