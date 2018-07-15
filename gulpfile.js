c2e4eea16a97bc59e5cec1d81bd4b6b58aa70c2c
Jay Kid <cyph3rfox@gmail.com>
2018-07-15 16:43:32 +0200

    feat: updated gulpfile to concat all js files within src

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
 var watch = require('gulp-watch');
 var uglify = require('gulp-uglify');
 var pump = require('pump');
@@ -8,35 +9,24 @@ var textTransformation = require('gulp-text-simple');
 // Very handcrafted (fragile) function to transform source into a copy-pastable bookmark :(
 var bookmarkify = function (fileContents) {
     var modifiedSource = fileContents;
-    modifiedSource = modifiedSource.slice(0,-3);
-    modifiedSource += ")();";
-    modifiedSource = modifiedSource.replace("!function","javascript:(function");
+    modifiedSource = "javascript:(function(){" + modifiedSource + "})();";
     return modifiedSource;
 }
 
 var wrappedBookmarkify = textTransformation(bookmarkify);
 
-gulp.task('compress', function (cb) {
-  pump([
-        gulp.src('main.js'),
-        uglify(),
-        gulp.dest('dist')
-    ],
-    cb
-  );
-});
- 
-gulp.task('bookmarkify', function() {
-  return gulp.src(['./dist/main.js'])
-    .pipe(wrappedBookmarkify())
-    .pipe(gulp.dest('dist/'));
+gulp.task('watch', function () {
+  return watch('src/*.js', function () {
+      return gulp.src('src/*.js')
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('temp/'));
+  });
 });
 
- 
-gulp.task('watch', function () {
-  return watch('main.js', function () {
-      return gulp.src('main.js')
-        .pipe(gulp.dest('temp/'));
-      
-  });
+gulp.task('build', function () {
+    return gulp.src('src/*.js')
+        .pipe(concat('main.js'))
+        .pipe(uglify('main.js'))
+        .pipe(wrappedBookmarkify())
+        .pipe(gulp.dest('dist/'));
 });
