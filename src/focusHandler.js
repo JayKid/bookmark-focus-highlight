db9aad04458e2a7fabba33ed9e088ea1d26d809b
Jay Kid <cyph3rfox@gmail.com>
2018-07-17 21:51:33 +0200

    chore: "port" FocusHandler to es6

--- src/focusHandler.js
+++ src/focusHandler.js
@@ -1,43 +1,43 @@
 // FocusHandler
 // Constants
-var COLORS = ['lime','rebeccapurple','orange','blue'];
-var COLOR_TOKEN = 'COLOR';
-var STYLE_TAG_CONTENTS_TEMPLATE = "*:focus {border: 5px solid " + COLOR_TOKEN + " !important;} input[type=\"radio\"]:focus, input[type=\"checkbox\"]:focus { outline: 5px solid " + COLOR_TOKEN + " !important; }";
-var STYLE_TAG_ID = 'bfh';
-var ZERO_KEYCODE = 48;
+const COLORS = ['lime','rebeccapurple','orange','blue']
+const COLOR_TOKEN = 'COLOR'
+const STYLE_TAG_CONTENTS_TEMPLATE = `*:focus {border: 5px solid ${COLOR_TOKEN} !important;} input[type="radio"]:focus, input[type="checkbox"]:focus { outline: 5px solid ${COLOR_TOKEN} !important; }`
+const STYLE_TAG_ID = 'bfh'
+const ZERO_KEYCODE = 48
 
 // State
-var currentColorIndex = 0;
+let currentColorIndex = 0
 
 // Methods
-var getNextColor = function () {
-    currentColorIndex = (currentColorIndex+1) % COLORS.length;
-    return COLORS[currentColorIndex];
+const getNextColor = () => {
+    currentColorIndex = (currentColorIndex+1) % COLORS.length
+    return COLORS[currentColorIndex]
 };
 
-var generateStyleTagContents = function (color) {
-    var contents = STYLE_TAG_CONTENTS_TEMPLATE.replace(COLOR_TOKEN, color).replace(COLOR_TOKEN, color);
-    return decodeURIComponent(contents);
+const generateStyleTagContents = color => {
+    const contents = STYLE_TAG_CONTENTS_TEMPLATE.replace(COLOR_TOKEN, color).replace(COLOR_TOKEN, color)
+    return decodeURIComponent(contents)
 };
 
-var createAndAppendStyleTag = function () {
-    var styleTag = document.createElement('style');
-    styleTag.setAttribute('id', STYLE_TAG_ID);
-    var nextColor = getNextColor();
-    styleTag.innerHTML = generateStyleTagContents(nextColor);
-    document.querySelector('head').appendChild(styleTag);
-};
+const createAndAppendStyleTag = () => {
+    let styleTag = document.createElement('style')
+    styleTag.setAttribute('id', STYLE_TAG_ID)
+    const nextColor = getNextColor()
+    styleTag.innerHTML = generateStyleTagContents(nextColor)
+    document.querySelector('head').appendChild(styleTag)
+}
 
-var toggleColor = function (event) {
+const toggleColor = event => {
     if (event && event.keyCode === ZERO_KEYCODE) {
-        var currentStyleTag = document.querySelector('#'+STYLE_TAG_ID);
-        var nextColor = getNextColor();
-        currentStyleTag.innerHTML = generateStyleTagContents(nextColor);
+        let currentStyleTag = document.querySelector(`#${STYLE_TAG_ID}`)
+        const nextColor = getNextColor()
+        currentStyleTag.innerHTML = generateStyleTagContents(nextColor)
     }
-};
+}
 
-var initializeKeyBindings = function () {
-    document.addEventListener('keyup', toggleColor);
+const initializeKeyBindings = () => {
+    document.addEventListener('keyup', toggleColor)
 }
 
 window.FocusHandler = {
