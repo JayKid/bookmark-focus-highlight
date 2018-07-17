fe520b0278966d5172bc9fe7577e8cc5b2cc28c1
Jay Kid <cyph3rfox@gmail.com>
2018-07-17 21:01:23 +0200

    chore: set up attempt of babel preset in Gulp
    Will tweak exact settings later :see_no_evil:

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var watch = require('gulp-watch');
 var uglify = require('gulp-uglify');
+var babel = require('gulp-babel');
 var textTransformation = require('gulp-text-simple');
 
 // Very handcrafted (fragile) function to transform source into a copy-pastable bookmark :(
@@ -17,6 +18,16 @@ gulp.task('watch', function () {
     return watch('src/*.js', function () {
         return gulp.src('src/*.js')
           .pipe(concat('main.js'))
+          .pipe(babel({
+              presets: [
+                ["env", {
+                  "targets": {
+                    // The % refers to the global coverage of users from browserslist
+                    "browsers": [ ">0.25%", "not ie 11", "not op_mini all"]
+                  }
+                }]
+              ]
+          }))
           .pipe(gulp.dest('temp/'));
     });
 });
@@ -24,7 +35,16 @@ gulp.task('watch', function () {
 gulp.task('build', function () {
     return gulp.src('src/*.js')
         .pipe(concat('main.js'))
-        .pipe(uglify('main.js'))
+        .pipe(babel({
+            presets: [
+                ["env", {
+                  "targets": {
+                    // The % refers to the global coverage of users from browserslist
+                    "browsers": [ ">0.25%", "not ie 11", "not op_mini all"]
+                  }
+                }]
+              ]
+        }))
         .pipe(wrappedBookmarkify())
         .pipe(gulp.dest('dist/'));
 });
